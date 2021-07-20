3309277f1db1de1aaf660622a4c918228c049b52
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-20 09:44:42 +0200

    add aware

---
+++ src/arrayJs/awareArr.js
@@ -0,0 +1,28 @@
+
+
+export const awarelist = [
+    {
+        img : 'award-1.png',
+        text : 'Winner Seo Master MAGT Smart Start Award 2017',
+        name : 'Winner Seo Master',
+        filter : 'invert(59%) sepia(92%) saturate(547%) hue-rotate(164deg) brightness(96%) contrast(83%);'
+    },
+    {
+        img : 'award-2.png',
+        text : 'Top Social Media Agencies Next Partner 2018',
+        name : 'Top Social Media',
+        filter : 'none;'
+    },
+    {
+        img : 'award-3.png',
+        text : '10 Fastest Growing Abstract Solution Providers 2019',
+        name : 'Growing Abstract',
+        filter: 'invert(58%) sepia(67%) saturate(1915%) hue-rotate(319deg) brightness(98%) contrast(103%);'
+    },
+    {
+        img : 'award-4.png',
+        text : 'National Excellence Agencie Award Winner 2020',
+        name : 'Excellence Agencie',
+        filter: 'invert(56%) sepia(54%) saturate(3416%) hue-rotate(204deg) brightness(102%) contrast(103%);'
+    },
+]
---
+++ src/components/CardAware.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-align-center p-3">
+    <div>
+      <img :src="require('@/assets/imgAware/' + trophy.img)" :alt="trophy.name" :style=" 'filter :' +trophy.filter">
+    </div>
+    <span>{{trophy.text}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'CardAware',
+    props : ['trophy'],
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '@/styles/flex.scss';
+    @import '@/styles/buttons.scss';
+  img {
+    width: 150px;    
+  }
+</style>
